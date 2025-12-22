1c9b0b2b8af340a5c3b811fa813366c6a553c7c5
Paul Ged <pauliusgedrimas@gmail.com>
2025-12-21 19:16:05 -0800

    fixed case sensitive issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Auth from "./pages/Auth";
 import Trace from "./pages/Trace";
 import AdminAuth from "./pages/AdminAuth";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/DashBoard";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AdminProtectedRoute from "./components/AdminProtectedRoute";
 
