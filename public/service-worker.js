d629c7eccf31817dfe169e799edb92b6c77f4586
Paul Ged <pauliusgedrimas@gmail.com>
2025-12-23 20:59:55 -0800

    added push notifications fixed bugs on server timings

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,15 @@
-self.addEventListener("push", event => {
+self.addEventListener("push", (event) => {
   const data = event.data.json();
-
   event.waitUntil(
-    self.registration.showNotification("Status Check", {
-      body: `Do you still feel ${data.label_name}?`,
-      data: {
-        state_id: data.state_id
-      }
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      data: data.data,
+      icon: "/icons/icon-192.png",
     })
   );
 });
 
-self.addEventListener("notificationclick", event => {
+self.addEventListener("notificationclick", (event) => {
   event.notification.close();
-  event.waitUntil(
-    clients.openWindow("/trace")
-  );
+  event.waitUntil(clients.openWindow("/trace"));
 });
